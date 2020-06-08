ff5d3e77ba273cf85e7b7c95b7d1af108629147c
mferoc <matheusferreiradeocosta@gmail.com>
2020-06-08 17:02:55 -0300

    operators

---
+++ capitulo02/demo03-operadoes.js
@@ -0,0 +1,20 @@
+/**
+ * arithmetic operators: 
+ * + addition 2 + 2 = 4
+ * - subtraction  2 - 2 = 0
+ * * multiplication 2 * 2 = 4
+ * / division 2 / 2 = 1
+ * % remainder 6 % 4 = 2 look 6 / 4 = 1.5 
+ * ** exponentiation 2 ** 3 = 8 look 2 ^ 3 = 2 * 2 * 2 = 8 
+ */
+/**
+ * comparision operators:
+ * < less than
+ * > greater than
+ * === equal value and equal type
+ * !== not equal value and not equal type
+ * <= less than or equal to
+ * >= greater than or equal to
+ * || or
+ * && and
+ */
